2d070c2bb9261799b0e64259a49555ceb3198c03
SK\ssssk <ssssksss@naver.com>
2023-10-06 04:48:50 +0900

    [fix]

--- src/components/EconomicPerformanceEvaluation.tsx
+++ src/components/EconomicPerformanceEvaluation.tsx
@@ -102,7 +102,12 @@ const EconomicPerformanceEvaluation = (props: any) => {
           <Line options={options} data={data} />
         </RateBox>
         <RatingBox>
-          <TitleBox> 투자회수기간 </TitleBox>
+          <TitleBox>
+            {' '}
+            투자회수
+            <br />
+            기간{' '}
+          </TitleBox>
           <div> {labelsData?.length ? labelsData?.length : 0} 년 </div>{' '}
         </RatingBox>
       </CC.RowDiv>
